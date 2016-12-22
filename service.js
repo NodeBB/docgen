d660a2194585d8a6a1c83b06759b0d913b1a49b3
psychobunny <rodrigues.andrew@gmail.com>
2016-12-21 21:19:58 -0500

    add route for getting hooks

--- service.js
+++ service.js
@@ -52,6 +52,10 @@ app.post('/push', function (req, res) {
 	});
 });
 
+app.get('/hooks', function (req, res) {
+	res.json(require('./lib/documenter').docs);
+});
+
 var server = app.listen(5000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
